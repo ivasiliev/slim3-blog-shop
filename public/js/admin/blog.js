3324b02c86a234cb4f09cfd9aa11e7f64f3fba71
ivasiliev <ivasiliev@169.254.1.235>
2017-08-30 18:29:44 +0300

    add

--- public/js/admin/blog.js
+++ public/js/admin/blog.js
@@ -4,24 +4,36 @@ var blog = {
         api_url_update: '/api/blog/update',
         api_url_info: '/api/blog/info',
         api_url_drop: '/api/blog/drop',
-        
+
         api_url_categories_create: '/api/blog/categories/save',
         api_url_categories_update: '/api/blog/categories/update',
         api_url_categories_info: '/api/blog/categories/info',
         api_url_categories_drop: '/api/blog/categories/drop',
-        
+
         arr: [],
         tagname: "blog_data",
         add_tagname: "blog_add_data",
         update_tagname: "blog_upd_data",
-        
-        showCategories: function(elem){
-                if (elem){
+
+        showCategories: function (elem) {
+                if (elem) {
                         menu_action(elem);
                 }
                 this.send('GET', null, this.api_url_categories_info, this.get_subcontent_elem());
                 return this;
+        },
+
+        open_create: function () {
+                var self = this;
+                this.send('GET', null, this.api_url_categories_info, function (data, elem) {
+                        var obj = self;
+                        obj.open_create_modal(data, elem);
+                });
+        },
+        
+        open_create_modal: function(data, elem){
+                modal.show(data);
         }
-        
-        
+
+
 };
