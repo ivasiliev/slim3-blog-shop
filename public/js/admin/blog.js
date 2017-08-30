e1adf5e4d2e1d1d6016f7bf1b9a2c25691e429df
ivasiliev <ivasiliev@169.254.1.235>
2017-08-30 20:38:06 +0300

    add

--- public/js/admin/blog.js
+++ public/js/admin/blog.js
@@ -38,7 +38,7 @@ var blog = {
 };
 
 var category = {
-        __proto__: data_proto,
+        __proto__: blog,
 
         api_url_form: '/api/blog/categories/form/',
         api_url_save: '/api/blog/categories/save',
@@ -52,34 +52,6 @@ var category = {
                         menu_action(elem);
                 }
                 this.send('GET', null, this.api_url_info, this.get_subcontent_elem());
-                return this;
-        },
-
-        open_form: function (id) {
-                var self = this;
-                this.send('GET', null, this.api_url_form + (id ? id : ''), null, null, function (data, elem) {
-                        var obj = self;
-                        obj.open_form_modal(data, elem);
-                });
-        },
-
-        open_form_modal: function (data, elem) {
-                modal.show(data);
-        },
-
-        save: function (elem) {
-                var cont = elem.parentNode.parentNode;
-                if (elem) {
-                        menu_action(elem);
-                }
-                var form = this.create_reqdata(cont);
-                if (form) {
-                        this.send('POST', form, this.api_url_save, this.get_subcontent_elem(), cont);
-                }
-        },
-        
-        drop: function (id) {
-                this.send('GET', null, this.api_url_drop + id, this.get_subcontent_elem());
                 return this;
         }
 };
