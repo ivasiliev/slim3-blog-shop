e28f802066186e16b56262a3b1f875816830e43a
ivasiliev <ivasiliev@169.254.1.235>
2017-09-06 21:12:00 +0300

    add

--- app/src/Action/UserAction.php
+++ app/src/Action/UserAction.php
@@ -8,8 +8,9 @@ use Slim\Http\Request;
 use Slim\Http\Response;
 use App\Action\Imgs;
 use App\Action\Auth;
+use App\ORM\DataService;
 
-final class UserAction {
+final class UserAction extends DataService {
 
         private $view;
         private $logger;
@@ -17,6 +18,7 @@ final class UserAction {
         private $user;
 
         public function __construct(Twig $view, LoggerInterface $logger) {
+                parent::__construct($view, $logger);
                 $this->view = $view;
                 $this->logger = $logger;
                 $this->path = __DIR__ . "/../../../public/css/photo/";
@@ -24,6 +26,7 @@ final class UserAction {
         }
 
         public function __invoke(Request $request, Response $response, $args) {
+                $this->view->render($response, 'main.twig', array());
                 return $response;
         }
 
@@ -51,6 +54,81 @@ final class UserAction {
                         return $response->withJson(array("result" => 200, "content" => array("type" => "success", "sid" => $userdata["session_id"])));
                 }
                 return $response->withJson(array("result" => 400, "content" => "unknown"));
+        }
+        
+        //----------------------------------------------------------------------
+        // admin methods
+        //----------------------------------------------------------------------
+        
+        public function AdminView(Request $request, Response $response, $args) {
+                $data = $this->getUsersData();
+                $this->view->render($response, 'admin/users/main.twig', array(
+                    "categories"=>$data
+                ));
+                return $response;
+        }
+        
+        public function AdminForm(Request $request, Response $response, $args) {
+                if (isset($args["curr_id"]) && $args["curr_id"] !== ""){
+                        $data = $this->getUsersData($args["curr_id"]);
+                }
+                else {
+                        $data = array();
+                }
+                $this->view->render($response, 'admin/users/form.twig', array(
+                    "data"=>$data
+                ));
+                return $response;
+        }
+        
+        public function AdminSave(Request $request, Response $response, $args) {
+                $params = $request->getParsedBody();
+                if (!$params){
+                        return $response->withStatus(400, "empty request");
+                }
+                
+                $curr_id = null;
+                if (isset($params["curr_id"]) && $params["curr_id"]){
+                        $curr_id = $params["curr_id"];
+                }
+                
+                if (!$curr_id){
+                        $curr_id = uniqid();
+                }
+                $elem = array(
+                    "id" => $curr_id,
+                    "name" => $params["name"],
+                    "descr" => $params["descr"],
+                );
+                
+                $list = $this->getCategoryData();
+                $list[$curr_id] = $elem;
+                
+                // save datafile
+                $this->saveCategoryData($list);
+                
+                // return rendered data
+                return $this->AdminView($request, $response, $args);
+        }
+        
+        public function AdminDrop(Request $request, Response $response, $args) {
+                if (!(isset($args["curr_id"]) && $args["curr_id"] !== "")){
+                        return $response->withStatus(400, "empty request");
+                }
+                
+                $curr_id = $args["curr_id"];
+                
+                $list = $this->getCategoryData();
+                if (isset($list[$curr_id])){
+                        // remove current data
+                        unset($list[$curr_id]);
+                }
+                
+                // save datafile
+                $this->saveCategoryData($list);
+                
+                // return rendered data
+                return $this->AdminView($request, $response, $args);
         }
 
 }
--- public/js/admin/users.js
+++ public/js/admin/users.js
@@ -1,11 +1,43 @@
 var users = {
         __proto__: data_proto,
-        api_url_create: '/api/stub',
-        api_url_update: '/api/stub',
-        api_url_info: '/api/stub',
-        api_url_drop: '/api/stub',
+        api_url_form: '/api/users/form/',
+        api_url_save: '/api/users/save',
+        api_url_info: '/api/users/info',
+        api_url_drop: '/api/users/drop/',
         arr: [],
-        tagname: "blog_data",
-        add_tagname: "blog_add_data",
-        update_tagname: "blog_upd_data"
+        tagname: "users_data",
+        add_tagname: "users_add_data",
+        update_tagname: "users_upd_data",
+        
+        arr: [],
+
+        open_form: function (id) {
+                var self = this;
+                this.send('GET', null, this.api_url_form + (id ? id : ''), null, null, function (data, elem) {
+                        var obj = self;
+                        obj.open_form_modal(data, elem);
+                });
+        },
+
+        open_form_modal: function (data, elem) {
+                modal.show(data);
+        },
+
+        save: function (elem) {
+                var cont = elem.parentNode.parentNode;
+                if (elem) {
+                        menu_action(elem);
+                }
+                var form = this.create_reqdata(cont);
+                if (form) {
+                        this.send('POST', form, this.api_url_save, this.get_subcontent_elem(), cont);
+                }
+        },
+
+        drop: function (id) {
+                if (confirm("Вы уверены в удалении?")) {
+                        this.send('GET', null, this.api_url_drop + id, this.get_subcontent_elem());
+                }
+        }
+        
 };
