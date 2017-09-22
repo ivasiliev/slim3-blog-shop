15d4d74bb34c35fc610d28b9af8d577ccac5dbd1
ivasiliev <ivasiliev@169.254.1.235>
2017-09-22 19:57:40 +0300

    add

--- public/js/main.js
+++ public/js/main.js
@@ -577,7 +577,7 @@ var comments = {
                         if (!this.arr.hasOwnProperty(key)) {
                                 continue;
                         }
-                        if (String(parent_id) === String(arr[key].parent_id)) {
+                        if (String(parent_id) === String(this.arr[key].parent_id)) {
                                 str += this._getCurrContent(this.arr[key]);
                                 str += '<div class="comment_childs_list">';
                                 if (parent_id) {
